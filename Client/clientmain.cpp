9fb74d431a9d0e86416331121691cbb30b63c2a6
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-11-20 19:27:45 +0300

    enable log in release

--- Client/clientmain.cpp
+++ Client/clientmain.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     qInstallMessageHandler(messageHandler);
+    qInfo() << "--- launch client ---";
 
     ChatWindow chatWin;
     chatWin.show();
