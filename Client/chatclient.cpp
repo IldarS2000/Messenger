d30568f31dcd1ef7173eb2f367dead77635e254b
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-11-20 21:38:49 +0300

    change client back

--- Client/chatclient.cpp
+++ Client/chatclient.cpp
@@ -7,50 +7,50 @@
 
 ChatClient::ChatClient(QObject* parent) : QObject(parent), clientSocket(new QTcpSocket(this)), logged(false)
 {
-    // Forward the connected and disconnected signals
     connect(clientSocket, &QTcpSocket::connected, this, &ChatClient::connected);
     connect(clientSocket, &QTcpSocket::disconnected, this, &ChatClient::disconnected);
-    // connect readyRead() to the slot that will take care of reading the data in
+    connect(clientSocket, &QTcpSocket::disconnected, this, [this]() -> void { logged = false; });
+
     connect(clientSocket, &QTcpSocket::readyRead, this, &ChatClient::onReadyRead);
-    // Forward the error signal, QOverload is necessary as error() is overloaded, see the Qt docs
     connect(clientSocket, QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::error), this,
             &ChatClient::error);
-    // Reset the m_loggedIn variable when we disconnec. Since the operation is trivial we use a lambda instead of
-    // creating another slot
-    connect(clientSocket, &QTcpSocket::disconnected, this, [this]() -> void { logged = false; });
+}
+
+ChatClient::~ChatClient()
+{
+    delete clientSocket;
+}
+
+void ChatClient::connectToServer(const QHostAddress& address, const quint16 port)
+{
+    clientSocket->connectToHost(address, port);
 }
 
 void ChatClient::login(const QString& userName)
 {
-    if (clientSocket->state() == QAbstractSocket::ConnectedState) { // if the client is connected
-        // create a QDataStream operating on the socket
+    if (clientSocket->state() == QAbstractSocket::ConnectedState) {
         QDataStream clientStream(clientSocket);
-        // set the version so that programs compiled with different versions of Qt can agree on how to serialise
         clientStream.setVersion(serializerVersion);
-        // Create the JSON we want to send
-        QJsonObject message;
-        message["type"] = "login";
-        message["username"] = userName;
-        // send the JSON using QDataStream
-        clientStream << QJsonDocument(message).toJson(QJsonDocument::Compact);
+
+        QJsonObject dataUnit;
+        dataUnit["type"] = "login";
+        dataUnit["username"] = userName;
+        clientStream << QJsonDocument(dataUnit).toJson(QJsonDocument::Compact);
     }
 }
 
-void ChatClient::sendMessage(const QString& text)
+void ChatClient::sendMessage(const QString& message)
 {
-    if (text.isEmpty()) {
-        return; // We don't send empty messages
+    if (message.isEmpty()) {
+        return;
     }
-    // create a QDataStream operating on the socket
     QDataStream clientStream(clientSocket);
-    // set the version so that programs compiled with different versions of Qt can agree on how to serialise
     clientStream.setVersion(serializerVersion);
-    // Create the JSON we want to send
-    QJsonObject message;
-    message["type"] = "message";
-    message["text"] = text;
-    // send the JSON using QDataStream
-    clientStream << QJsonDocument(message).toJson();
+
+    QJsonObject dataUnit;
+    dataUnit["type"] = "message";
+    dataUnit["text"] = message;
+    clientStream << QJsonDocument(dataUnit).toJson();
 }
 
 void ChatClient::disconnectFromHost()
@@ -58,102 +58,70 @@ void ChatClient::disconnectFromHost()
     clientSocket->disconnectFromHost();
 }
 
-void ChatClient::jsonReceived(const QJsonObject& docObj)
+void ChatClient::jsonReceived(const QJsonObject& dataUnit)
 {
-    // actions depend on the type of message
-    const QJsonValue typeVal = docObj.value(QLatin1String("type"));
+    const QJsonValue typeVal = dataUnit.value(QLatin1String("type"));
     if (typeVal.isNull() || !typeVal.isString()) {
-        return; // a message with no type was received so we just ignore it
+        return;
     }
-    if (typeVal.toString().compare(QLatin1String("login"), Qt::CaseInsensitive) == 0) { // It's a login message
+    if (typeVal.toString().compare(QLatin1String("login"), Qt::CaseInsensitive) == 0) {
         if (logged) {
-            return; // if we are already logged in we ignore
+            return;
         }
-        // the success field will contain the result of our attempt to login
-        const QJsonValue resultVal = docObj.value(QLatin1String("success"));
+        const QJsonValue resultVal = dataUnit.value(QLatin1String("success"));
         if (resultVal.isNull() || !resultVal.isBool()) {
-            return; // the message had no success field so we ignore
+            return;
         }
         const bool loginSuccess = resultVal.toBool();
         if (loginSuccess) {
-            // we logged in succesfully and we notify it via the loggedIn signal
             emit loggedIn();
             return;
         }
-        // the login attempt failed, we extract the reason of the failure from the JSON
-        // and notify it via the loginError signal
-        const QJsonValue reasonVal = docObj.value(QLatin1String("reason"));
+        const QJsonValue reasonVal = dataUnit.value(QLatin1String("reason"));
         emit loginError(reasonVal.toString());
-    } else if (typeVal.toString().compare(QLatin1String("message"), Qt::CaseInsensitive) == 0) { // It's a chat message
-        // we extract the text field containing the chat text
-        const QJsonValue textVal = docObj.value(QLatin1String("text"));
-        // we extract the sender field containing the username of the sender
-        const QJsonValue senderVal = docObj.value(QLatin1String("sender"));
+    } else if (typeVal.toString().compare(QLatin1String("message"), Qt::CaseInsensitive) == 0) {
+        const QJsonValue textVal = dataUnit.value(QLatin1String("text"));
         if (textVal.isNull() || !textVal.isString()) {
-            return; // the text field was invalid so we ignore
+            return;
         }
+        const QJsonValue senderVal = dataUnit.value(QLatin1String("sender"));
         if (senderVal.isNull() || !senderVal.isString()) {
-            return; // the sender field was invalid so we ignore
+            return;
         }
-        // we notify a new message was received via the messageReceived signal
         emit messageReceived(senderVal.toString(), textVal.toString());
-    } else if (typeVal.toString().compare(QLatin1String("newuser"), Qt::CaseInsensitive) ==
-               0) { // A user joined the chat
-        // we extract the username of the new user
-        const QJsonValue usernameVal = docObj.value(QLatin1String("username"));
+    } else if (typeVal.toString().compare(QLatin1String("newuser"), Qt::CaseInsensitive) == 0) {
+        const QJsonValue usernameVal = dataUnit.value(QLatin1String("username"));
         if (usernameVal.isNull() || !usernameVal.isString()) {
-            return; // the username was invalid so we ignore
+            return;
         }
-        // we notify of the new user via the userJoined signal
         emit userJoined(usernameVal.toString());
-    } else if (typeVal.toString().compare(QLatin1String("userdisconnected"), Qt::CaseInsensitive) ==
-               0) { // A user left the chat
-                    // we extract the username of the new user
-        const QJsonValue usernameVal = docObj.value(QLatin1String("username"));
+    } else if (typeVal.toString().compare(QLatin1String("userdisconnected"), Qt::CaseInsensitive) == 0) {
+        const QJsonValue usernameVal = dataUnit.value(QLatin1String("username"));
         if (usernameVal.isNull() || !usernameVal.isString()) {
-            return; // the username was invalid so we ignore
+            return;
         }
-        // we notify of the user disconnection the userLeft signal
         emit userLeft(usernameVal.toString());
     }
 }
 
-void ChatClient::connectToServer(const QHostAddress& address, quint16 port)
-{
-    clientSocket->connectToHost(address, port);
-}
-
 void ChatClient::onReadyRead()
 {
-    // prepare a container to hold the UTF-8 encoded JSON we receive from the socket
     QByteArray jsonData;
-    // create a QDataStream operating on the socket
     QDataStream socketStream(clientSocket);
-    // set the version so that programs compiled with different versions of Qt can agree on how to serialise
     socketStream.setVersion(serializerVersion);
-    // start an infinite loop
-    while(true) {
-        // we start a transaction so we can revert to the previous state in case we try to read more data than is
-        // available on the socket
+
+    while (true) {
         socketStream.startTransaction();
-        // we try to read the JSON data
         socketStream >> jsonData;
         if (socketStream.commitTransaction()) {
-            // we successfully read some data
-            // we now need to make sure it's in fact a valid JSON
             QJsonParseError parseError = {0};
-            // we try to create a json document with the data we received
             const QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonData, &parseError);
             if (parseError.error == QJsonParseError::NoError) {
-                // if the data was indeed valid JSON
-                if (jsonDoc.isObject()) { // and is a JSON object
-                    jsonReceived(jsonDoc.object()); // parse the JSON
+                if (jsonDoc.isObject()) {
+                    jsonReceived(jsonDoc.object());
                 }
             }
-            // loop and try to read more JSONs if they are available
         } else {
-            // the read failed, the socket goes automatically back to the state it was in before the transaction started
-            // we just exit the loop and wait for more data to become available
             break;
         }
     }
--- Client/chatclient.h
+++ Client/chatclient.h
@@ -12,18 +12,19 @@ class ChatClient : public QObject
     Q_DISABLE_COPY(ChatClient)
 public:
     explicit ChatClient(QObject* parent = nullptr);
+    ~ChatClient() override;
 public slots:
     void connectToServer(const QHostAddress& address, quint16 port);
     void login(const QString& userName);
-    void sendMessage(const QString& text);
+    void sendMessage(const QString& message);
     void disconnectFromHost();
 private slots:
     void onReadyRead();
 signals:
     void connected();
+    void disconnected();
     void loggedIn();
     void loginError(const QString& reason);
-    void disconnected();
     void messageReceived(const QString& sender, const QString& text);
     void error(QAbstractSocket::SocketError socketError);
     void userJoined(const QString& username);
@@ -32,7 +33,9 @@ signals:
 private:
     QTcpSocket* clientSocket;
     bool logged;
-    void jsonReceived(const QJsonObject& doc);
+
+private:
+    void jsonReceived(const QJsonObject& dataUnit);
 };
 
 #endif // CHATCLIENT_H
