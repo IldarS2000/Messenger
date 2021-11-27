b8b54975ac287ca0beb96debabf9ee9322f74214
Ildar <45717260+Karantir73@users.noreply.github.com>
2021-11-27 22:21:01 +0300

    add logo

--- Client/main.cpp
+++ Client/main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-   // app.setWindowIcon(QIcon("./Client/recources/appIcon"));
     qInstallMessageHandler(messageHandler);
 
     qInfo() << "--- launch client ---";
